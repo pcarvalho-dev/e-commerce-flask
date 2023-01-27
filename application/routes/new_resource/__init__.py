8b45456c427339e55d10eef685ed4273b5df123b
Pablo Carvalho <pablocarvalho.dev@gmail.com>
2023-01-27 08:53:59 -0300

    Revert "Teste"
    This reverts commit e7464b428b1cabde6c992f42ffecef473c8839ba.

--- application/routes/new_resource/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-teste_bp = Blueprint("teste_bp", __name__)
-
-from . import category
-
-
-def init_app(app):
-    app.register_blueprint(teste_bp, url_prefix="/v1/teste")
